057ad4df7022a5e43aac9a0d36113d6bb5e03017
lajos.pajtek <lajos.pajtek@localhost>
2010-04-06 15:12:35 +0200

    misc

--- p001.py
+++ p001.py
@@ -9,6 +9,13 @@ while n<limit:
 	n+=1
 
 a.sort()
+print "len:", len(a)
 print "sum:", sum(a)
 print "sum:", s
 #text = raw_input('press enter')
+
+# pencil-and-paper solution:
+# sum of multipliers of 3, below 1000: 3 * (333*(333+1))/2
+# sum of multipliers of 5, below 1000: 5 * (199*200)/2
+# minus the sum of multipliers of 15, below 1000: 15 * (66*67)/2
+
---
+++ p036.py
@@ -0,0 +1,16 @@
+
+def palindromic(s):
+    if s[-1] == '0': return False
+    n = len(s) // 2 +1
+    for i in range(0, n): 
+	if s[i] != s[-i-1]: return False
+    return True
+
+s = 0
+for i in range (1,1000000,2):
+    if palindromic(str(i)) and palindromic(bin(i)[2:]):
+        print i, bin(i)[2:]
+        s += i	
+
+print s
+
